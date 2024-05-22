d09029ee44172bdaf68405f3fa9500cf6a75761f
Vladislav <vmgit@yandex.ru>
2024-05-22 17:48:11 +0300

    clear2

--- base.py
+++ base.py
@@ -55,14 +55,14 @@ def clear_base():
     try:
         connection = con.connect(host="localhost", port=3306, user="root", password="root", database="mysql_links")
         if connection.is_connected():
-            print("Успешное подключение к базе данных")
+            print("Успешное подключение к базе данных mysql_links")
         cursor = connection.cursor()
-        delete_query = "DELETE FROM links WHERE expiry_at < NOW() LIMIT 1000"
-        cursor.execute(delete_query)
+        delete_query = "DELETE FROM links WHERE date < NOW() - INTERVAL %s SECOND LIMIT 1000"
+        cursor.execute(delete_query, (LifeTime,))
         connection.commit()
-        print("Успешно удалено строки из таблицы 'links'")
+        print("Успешно удалено строки из таблицы links")
     except con.Error as error:
-        print("Ошибка при удалении строк из таблицы 'links':", error)
+        print("Ошибка при удалении строк из таблицы links:", error)
     finally:
         if 'connection' in locals():
             connection.close()
